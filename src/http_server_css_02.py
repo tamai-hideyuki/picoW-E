595a90bb7ba619a7646143e92a258473415ad8a0
tamai-hideyuki <hide27yuki@gmail.com>
2026-04-04 08:28:38 +0900

    【プロパティについて学ぶ】mergin/padding/mergin-bottom/margin-top

---
+++ src/http_server_css_02.py
@@ -0,0 +1,87 @@
+import ssd1306
+import network
+import socket
+import time
+from machine import Pin, I2C
+
+SSID = "Wi-Fi名"
+PASSWORD = "パスワード"
+
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect(SSID, PASSWORD)
+
+while not wlan.isconnected():
+    time.sleep(1)
+
+ip = wlan.ifconfig()[0]
+
+time.sleep(2)
+
+i2c = I2C(0, sda=Pin(0), scl=Pin(1))
+oled = ssd1306.SSD1306_I2C(128, 64, i2c)
+
+oled.fill(0)
+oled.text("Connecting...", 0, 0)
+oled.show()
+
+
+oled.fill(0)
+oled.text("Connected!", 0, 0)
+oled.text(ip, 0, 16)
+oled.show()
+
+html = """\
+HTTP/1.0 200 OK
+Content-Type: text/html
+
+<html>
+<style>
+    body {
+        background-color: black;
+        color: white;
+        font-family: Arial, sans-serif;
+        text-align: center;
+        mergin: 0;
+        padding: 20px;
+    }
+
+    h1 {
+        text-decoration: underline;
+        mergin-botton: 40px;
+    }
+
+    h6 {
+        color: gray;
+        mergin-top: 40px;
+    }
+</style>
+<body>
+    <h1>Hello!</h1>
+    <h2>Hello!</h2>
+    <h3>Hello!</h3>
+    <h4>Hello!</h4>
+    <h5>Hello!</h5>
+    <h6>Hello!</h6>
+</body>
+</html>
+"""
+
+addr = socket.getaddrinfo("0.0.0.0", 80)[0][-1]
+s = socket.socket()
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind(addr)
+s.listen(1)
+
+oled.fill(0)
+oled.text("Server ready", 0, 0)
+oled.text(ip, 0, 16)
+oled.show()
+
+print("サーバー起動:", ip)
+
+while True:
+    conn, addr = s.accept()
+    conn.recv(1024)
+    conn.sendall(html)
+    conn.close()
